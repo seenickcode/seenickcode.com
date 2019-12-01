7b0342eeb2e5de78f015a9f27c5caff70dac4d06
Nicholas Manning <nicholas.manning@gmail.com>
2019-11-30 19:23:41 -0500

    Style updates.

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,9 +4,37 @@
   </div>
 </template>
 
+<script>
+export default {
+  head: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Work+Sans:300'
+      }
+    ]
+  }
+}
+</script>
+
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+html,
+body {
+  margin: 0px;
+  padding: 5px;
+  font-family: 'Work Sans', 'Avenir', sans-serif;
+  font-size: 18px;
+  line-height: 26px;
+}
+a {
+  color: blue;
+  text-decoration: none;
+}
+a:visited {
+  color: blue;
+}
+a:hover {
+  color: red;
+  text-decoration: underline;
 }
 </style>
