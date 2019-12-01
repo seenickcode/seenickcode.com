abfd1ad5d407f8df8cdf8d24521d66ed85207b31
Nicholas Manning <nicholas.manning@gmail.com>
2019-11-30 19:20:43 -0500

    Added some homepage copy.

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,62 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        seenickcode.com
-      </h1>
-      <h2 class="subtitle">
-        My astounding Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
+      <p>
+        Hi, my name is Nick. I've been a software engineering mentor, writer,
+        software craftsman and entrepreneur for over 20 years.
+      </p>
+      <p>
+        As for extra curricular projects, I've founded the largest
+        <a href="https://www.meetup.com/nycgolang" target="_blank"
+          >Golang meetup</a
         >
-          GitHub
-        </a>
-      </div>
+        in the U.S. outside of Silicon Valley, as well as the
+        <a href="https://www.meetup.com/nycneo4j/" target="_blank">NYC Neo4j</a>
+        meetup. My wife and I design and write mobile apps in our free time,
+        such as
+        <a href="https://steadycalendar.com" target="_blank">Steady Calendar</a>
+        and <a href="http://brewswap.co" target="_blank">Brewswap</a>. I also
+        <a href="https://medium.com/@seenickcode" target="_blank">write</a>
+        about technology on a regular basis.
+      </p>
+
+      <p>
+        In the past, I've held engineering, tech lead and consulting positions
+        working with various Fortune 500 companies, including Adidas, Reebok,
+        Benefit Cosmetics and TaylorMade.
+      </p>
+
+      <p>
+        In 2018, I've moved back to the U.S. after living abroad in Germany for
+        two years. I currently live in NYC with my wife and son.
+      </p>
+
+      <p>
+        I can be contacted on
+        <a href="https://www.linkedin.com/in/nicholasmanning/" target="_blank"
+          >LinkedIn</a
+        >
+        or
+        <a href="https://twitter.com/seenickcode" target="_blank">Twitter</a>.
+      </p>
+
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-123985192-1"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []
+        function gtag() {
+          dataLayer.push(arguments)
+        }
+        gtag('js', new Date())
+        gtag('config', 'UA-123985192-1')
+      </script>
     </div>
   </div>
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
+<script></script>
 
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
+<style></style>
